35def865593fcc1069791faba0da8b533021f37c
唐一钧 <m15524509248@163.com>
2022-10-16 12:26:38 +0800

    feat: 添加绘制矩形实例

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# canvas_learning
-由浅入深掌握canvas开发
+# Canvas_learning
+
+该系列是canvas的练手项目，利用canvas完成简单的图形绘制(矩形等基本图形)->简单的实例(时钟等图形)->小游戏(五子棋，flappy bird)/海报绘制等基本的场景。
+
+该项目的背景是帮助canvas的初学者在初学canvas过程中可以通过实际的案例快速上手并进一步开发出更有趣的项目！
+
+## Canvas基础知识
+
+关于canvas的基础知识大家应该都有一定的了解，这里不加赘述，想学习的同学可以参考
+
+* 《JavaScript高级程序设计(第4版)》第18章 动画与Canvas图形
+* 掘金上的优秀文章 《[案例+图解带你一文读懂canvas](https://juejin.cn/post/7119495608938790942)》《[Canvas从入门到劝朋友放弃》](https://juejin.cn/post/7116784455561248775)
+
+## Chapter1 Canvas简单图形绘制
---
+++ chapter1/绘制矩形/index.js
@@ -0,0 +1,58 @@
+//关键api: fillStyle, strokeStyle, fillRect, strokeRect, clearRect
+
+// 绘制从坐标(10,10)开始, 长宽都为50的红色矩形
+/*
+const drawRedRect = () => {
+  const drawing = document.getElementById('drawing');
+  const context = drawing.getContext('2d');
+  // 设置填充颜色
+  context.fillStyle = 'red';
+  context.fillRect(10, 10, 50, 50);
+}
+
+// 绘制从坐标(30,30)开始, 长宽都为50的半透明蓝色矩形
+const drawBlueRect = () => {
+  const drawing = document.getElementById('drawing');
+  const context = drawing.getContext('2d');
+  // 设置填充颜色
+  context.fillStyle = "rgba(0,0,255,0.5)";
+  context.fillRect(30, 30, 50, 50);
+}
+
+drawRedRect();
+drawBlueRect();
+*/
+
+//绘制坐标(10,10)开始，长宽为50的线宽矩形
+/*
+const drawRedStrokeRect = () => {
+  const drawing = document.getElementById('drawing');
+  const context = drawing.getContext('2d');
+  // 设置边框颜色
+  context.strokeStyle = 'red';
+  context.strokeRect(10, 10, 50, 50);
+}
+
+const drawBlueStrokeRect = () => {
+  const drawing = document.getElementById('drawing');
+  const context = drawing.getContext('2d');
+  // 设置边框颜色
+  context.strokeStyle = 'rgba(0,0,255,0.5)';
+  context.strokeRect(30, 30, 50, 50);
+}
+
+drawRedStrokeRect();
+drawBlueStrokeRect();
+*/
+
+//矩形开孔效果
+const clearRect = () => {
+  const drawing = document.getElementById('drawing');
+  const context = drawing.getContext('2d');
+  context.fillStyle = 'red';
+  context.fillRect(10, 10, 50, 50);
+  context.strokeStyle = 'rgba(0,0,255,0.5)';
+  context.strokeRect(30, 30, 50, 50);
+  context.clearRect(40, 40, 10, 10);
+}
+clearRect();
